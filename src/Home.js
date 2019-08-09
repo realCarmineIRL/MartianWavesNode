3d8f677d37d89794ffde263bb7b4c6f0d4edad4c
realCarmineIRL <calicchiocarmine@gmail.com>
2019-08-09 01:06:34 +0100

    adding extra info

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ class Home extends Component {
           <br />
           <p>Address for leasing:</p>
           <p><strong>3PBRNq59cSHTwCEXBNFXvakyxadfftEs5y7</strong></p>
+          <br />
+          <p><strong>100%</strong> Payout weekly + <strong>100 Martian Tokens</strong> per mined block</p>
           </div>
       </div>
     );
